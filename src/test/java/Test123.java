c8e0693d720b291d7ceb29b766daae0124e29439
subha <subhanlitun@gmail.com>
2022-10-22 18:25:11 +0530

    Test12

--- src/test/java/Test123.java
+++ src/test/java/Test123.java
@@ -3,7 +3,8 @@ public class Test123 {
         int n = 10;
         for (int i = 2; i < n; i++) {
             System.out.println("Print a new number" + i);
-        }
 
+        }
+        System.out.println("Print a new number" );
     }
 }
