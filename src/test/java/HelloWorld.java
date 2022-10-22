07d8c12d07bb1f6ba68a8be7450c9674c5d43397
subha <subhanlitun@gmail.com>
2022-10-22 17:51:20 +0530

    Test

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String args[]){
+
+        printName();
+ }
+ public static void printName(){
+        int n=10;
+        for (int i=2;i<n;i++) {
+            System.out.println("Print a new number" +i);
+        }
+
+
+
+
+
+
+
+
+
+
+ }
+}
+
---
+++ src/test/java/Test123.java
@@ -0,0 +1,9 @@
+public class Test123 {
+    public static void printName() {
+        int n = 10;
+        for (int i = 2; i < n; i++) {
+            System.out.println("Print a new number" + i);
+        }
+
+    }
+}
